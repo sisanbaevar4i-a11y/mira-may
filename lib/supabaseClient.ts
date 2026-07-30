0f17c7c16b94c86ce923625943eb0e713d55307b
Arthur <sisanbaev.ar4i@gmail.com>
2026-07-30 21:49:24 +0300

    fix supabase keys

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://uqznrjvponrpwccsfzpo.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_oVW9ZWCXnv4YN8oqaIj1bQ_OZqf3QIA';
+const supabaseUrl = 'https://uqznrjvponrpwccsfzpo.supabase.co';
+const supabaseKey = 'sb_publishable_oVW9ZWCXnv4YN8oqaIj1bQ_OZqf3QIA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
